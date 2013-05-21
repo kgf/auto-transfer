42e2745fdbc2fd6857f758e995e5f53bc1476e0b
Kevin <kfallon@seneschal.com>
2013-05-21 13:50:02 -0400

    Added rsync to transfer.sh

--- transfer.sh
+++ transfer.sh
@@ -4,3 +4,6 @@
 # KGF 5/1/2013
 echo "Current date : $(date) @ $(hostname)"
 
+#This is a first pass at what the rsync might look like. May be too much UI sruff here, like verbose, progress and human-readable
+#rsync --verbose --progress --stats --compress  --rsh=/usr/bin/ssh  --recursive --times --perms --links  --exclude 'cache' --exclude 'tmp' /data/*.jpg car3@10.101.54.3:/data/car3/ --human-readable
+
