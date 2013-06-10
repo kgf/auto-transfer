b692868c387924e196dcd5135f5b7e6f671d3333
Kevin Fallon <kfallon@seneschal.com>
2013-06-10 16:20:06 -0400

    Made call to Ruby portable across distros

--- rtransfer.rb
+++ rtransfer.rb
@@ -1,4 +1,4 @@
-#!/usr/local/bin/ruby
+#! /usr/bin/env ruby
 #Exploring the option of using ruby instead of shell script. Allows easier processing and access to libraries for saving logs to sqlite perhaps.
 #Interesting blog on various ways of making system calls here ;
 #http://blog.jayfields.com/2006/06/ruby-kernel-system-exec-and-x.html
