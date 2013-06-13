e1d4dcedf50c98043f1d1d15992e36bf088d4827
Kevin Fallon <kfallon@seneschal.com>
2013-06-12 22:04:16 -0400

    Toying with different ideas in rtransfer.rb

--- rtransfer.rb
+++ rtransfer.rb
@@ -5,5 +5,11 @@
 #We'll be using the result = %x(rsync) format
 
 rsync_results = %x[rsync 2>&1]
-scan_results = %x[iwlist wlan0 scanning 2>&1]
-puts "Results were: #{rsync_result}"
+#scan_results = %x[iwlist wlan0 scanning 2>&1]
+scan_results = %x[iwlist wlan0 scan | egrep "Address|ESSID" | cut -d: -f2-]
+puts "Results were: #{scan_results}"
+scan_array = scan_results.split("\n")
+
+puts scan_array.inspect
+puts scan_array.class
+puts scan_array(8)
